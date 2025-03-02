289e76857de61ef905e116c75a3b4e4edf59feeb
Poppy Novita <poppynovitaa27@gmail.com>
2025-03-02 15:43:15 +0700

    first commit

--- LuasLingkaran.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def luas_lingkaran(jari_jari):
-    return math.pi * jari_jari ** 2
-
-r = float(input("Masukkan jari-jari lingkaran: "))
-luas = luas_lingkaran(r)
-print(f"Luas lingkaran adalah: {luas:.2f}")
